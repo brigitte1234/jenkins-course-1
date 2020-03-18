6e222bcd46bf7efc0d9b4e0173710610c6d27adc
brigitte1234 <33143166+brigitte1234@users.noreply.github.com>
2020-03-18 14:16:22 +0800

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -13,6 +13,6 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        bat "cmd.exe /c npm install"
+        batchFile("cmd.exe /c npm install")
     }
 }
